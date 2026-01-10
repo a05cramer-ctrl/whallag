a6b8944d231963a2b83e57e32f99bbb57233640c
Whalleah <whalleah@example.com>
2026-01-10 19:34:12 +0100

    Refactor configuration and documentation files by adding empty lines for improved readability; update contract address in App component.

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const subheadlines = [
 function App() {
   const [subheadline, setSubheadline] = useState(subheadlines[0])
   const [copied, setCopied] = useState(false)
-  const contractAddress = '6cZw2f3GfdAzbf8NqhnYgqsBpdeNQGm5TG7ibLaSpump'
+  const contractAddress = 'bQwB8QHsmWkUWg15ACQof4Eys7m9WHXwGGNPq1abonk'
 
   useEffect(() => {
     // Rotate subheadline randomly
